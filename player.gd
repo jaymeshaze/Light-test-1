a44ff7e6a3be4e0f8af970c312d0344505569d14
jaymeshaze <khjesusfreak05@gmail.com>
2024-07-26 18:55:13 -0700

    Initial commit

---
+++ player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+func _ready():
+	pass
+
+func _process(delta):
+	position += Input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed * delta
+	
